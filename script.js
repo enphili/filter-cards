edb5bda635ec664ba60af5c90d1edf2c7548e2b3
enphili <41166090+enphili@users.noreply.github.com>
2020-09-16 17:29:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+const cartWrapper = document.querySelector('.cart-wrapper'),
+      tagsWrapper = document.querySelector('.filter-wrapper'),
+      cards = cartWrapper.querySelectorAll('.card');
+
+const selectedTags = new Set();
+
+const activeTag = e => {
+  const target = e.target;
+  if (target.classList.contains('filter')) {
+    target.classList.toggle('checked');
+    const tag = target.dataset.color;
+    if (target.classList.contains('checked')) {
+      selectedTags.add(tag);
+    } else {
+      selectedTags.delete(tag);
+    }
+    if (selectedTags.size > 0) {
+			cartWrapper.textContent = '';
+			const newcards = [...cards].filter(card => selectedTags.has(card.dataset.color));
+			cartWrapper.append(...newcards);
+    } else if (selectedTags.size <= 0) {
+			cartWrapper.textContent = '';
+			cartWrapper.append(...cards);
+		}
+  }
+};
+
+tagsWrapper.addEventListener('click', activeTag);
